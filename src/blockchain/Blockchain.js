769c8faa73ef6f18a48801d7fe26541b7fc59324
Bobby <bobtri15@gmail.com>
2026-08-21 11:51:11 +0200

    feat: implement proof-of-work mining

--- src/blockchain/Blockchain.js
+++ src/blockchain/Blockchain.js
@@ -1,10 +1,32 @@
 import crypto from 'crypto';
 
 class Blockchain {
+  constructor() {
+    this.difficulty = process.env.NODE_ENV === 'test' ? 1 : 2;
+  }
+
   calculateHash(index, previousHash, transactions, nonce) {
     const data = index + previousHash + JSON.stringify(transactions) + nonce;
 
     return crypto.createHash('sha256').update(data).digest('hex');
+  }
+
+  mineBlock(index, previousHash, transactions) {
+    let nonce = 0;
+    let hash = this.calculateHash(index, previousHash, transactions, nonce);
+
+    const target = '0'.repeat(this.difficulty);
+
+    while (!hash.startsWith(target)) {
+      nonce++;
+
+      hash = this.calculateHash(index, previousHash, transactions, nonce);
+    }
+
+    return {
+      nonce,
+      hash,
+    };
   }
 }
 
